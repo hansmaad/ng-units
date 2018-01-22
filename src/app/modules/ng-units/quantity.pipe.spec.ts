741a1ea9cadd6271b66f968fb9086352e72fac52
hansmaad <hansmaad@gmail.com>
2018-01-22 14:26:53 +0100

    QuantityPipe should be impure to detect unit changes.

--- src/app/modules/ng-units/quantity.pipe.spec.ts
+++ src/app/modules/ng-units/quantity.pipe.spec.ts
@@ -1,6 +1,18 @@
 import { QuantityPipe } from './quantity.pipe';
 import { Quantity } from './quantity';
 import { length } from './quantities/length';
+import { Component } from '@angular/core';
+import { TestBed, ComponentFixture } from '@angular/core/testing';
+
+
+@Component({
+    template: `
+    <div>{{ value | ngQuantity:quantity:true}}</div>`
+})
+class QuantityPipeTestComponent { 
+    value = 1;
+    quantity;
+}
 
 describe('QuantityPipe', () => {
 
@@ -35,4 +47,36 @@ describe('QuantityPipe', () => {
         expect(pipe.transform(2, quantity, true)).toBe('2000meow mm');
     })
 
+    describe('changeDetection', () => {
+    
+        let fixture: ComponentFixture<QuantityPipeTestComponent>;
+        beforeEach(() => {
+            fixture = TestBed.configureTestingModule({
+                declarations: [ QuantityPipe, QuantityPipeTestComponent ]
+              })
+              .createComponent(QuantityPipeTestComponent);
+              fixture.detectChanges(); 
+        });
+    
+        it('should render', () => {
+            expect(fixture.debugElement.nativeElement.textContent).toContain('1');
+        });
+
+        it('should render on quantity change', () => {
+            fixture.componentInstance.quantity = quantity;
+            fixture.detectChanges();
+            expect(fixture.debugElement.nativeElement.textContent).toContain('1000 mm');
+        });
+
+        it('should render on unit change', () => {
+            fixture.componentInstance.quantity = quantity;
+            fixture.detectChanges();
+            fixture.componentInstance.quantity.selectUnit('cm');
+            fixture.detectChanges();
+            expect(fixture.debugElement.nativeElement.textContent).toContain('100 cm');
+        });
+    });
 });
+
+
+
--- src/app/modules/ng-units/quantity.pipe.ts
+++ src/app/modules/ng-units/quantity.pipe.ts
@@ -2,14 +2,13 @@ import { Pipe, PipeTransform } from '@angular/core';
 import { Quantity } from './quantity';
 
 @Pipe({
-  name: 'ngQuantity'
+  name: 'ngQuantity',
+  pure: false
 })
 export class QuantityPipe implements PipeTransform {
 
   transform(value: string|number, quantity?: Quantity, addSymbol?: boolean): any {
-
     return quantity ? quantity.print(quantity.fromBase(value), addSymbol) : value;
-    
   }
 
 }
