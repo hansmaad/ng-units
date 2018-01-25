65c9d4b0259844ef78cd920f9020b44b8c8ec656
hansmaad <hansmaad@gmail.com>
2018-01-25 20:45:41 +0100

    Add unit test for ngQuantity directive.

--- src/app/modules/ng-units/quantity.directive.spec.ts
+++ src/app/modules/ng-units/quantity.directive.spec.ts
@@ -1,5 +1,54 @@
 import { QuantityDirective } from './quantity.directive';
+import { Component } from '@angular/core';
+import { ComponentFixture, TestBed, async } from '@angular/core/testing';
+import { SystemOfUnits } from './system-of-units.service';
+import { FormsModule } from '@angular/forms';
+import { Quantity } from './quantity';
+import { length } from './quantities/length';
+import { By } from '@angular/platform-browser';
+
+
+@Component({
+    template: `
+    <input type="number" [(ngModel)]="value" [ngQuantity]="quantity" >`
+})
+class QuantityDirectiveTestComponent { 
+    value = 1;
+    quantity: Quantity;
+}
 
 describe('QuantityDirective', () => {
+
+    let fixture: ComponentFixture<QuantityDirectiveTestComponent>;
+    let input;
+
+    beforeEach(() => {
+        fixture = TestBed.configureTestingModule({
+            imports: [FormsModule],
+            declarations: [ QuantityDirective, QuantityDirectiveTestComponent ],
+            providers: [SystemOfUnits]
+          })
+          .createComponent(QuantityDirectiveTestComponent);
+          fixture.detectChanges(); 
+          input = fixture.debugElement.query(By.css('input'));
+    });
+
+    it('should not convert value, if quantity is undefined', async(() => {
+        fixture.componentInstance.value = 42;
+        fixture.detectChanges();
+        fixture.whenStable().then(() => {
+            expect(input.nativeElement.value).toBe('42');
+        });
+    }))
+
+    it('should not convert from model', async(() => {
+        fixture.componentInstance.quantity = new Quantity(length);
+        fixture.componentInstance.quantity.selectUnit('cm');
+        fixture.componentInstance.value = 42;
+        fixture.detectChanges();
+        fixture.whenStable().then(() => {
+            expect(input.nativeElement.value).toBe('4200');
+        });
+    }))
 
 });
--- src/app/modules/ng-units/quantity.directive.ts
+++ src/app/modules/ng-units/quantity.directive.ts
@@ -1,11 +1,11 @@
 import {
-  Directive,
-  ElementRef,
-  forwardRef,
-  HostListener,
-  Input,
-  OnInit,
-  DoCheck
+    Directive,
+    ElementRef,
+    forwardRef,
+    HostListener,
+    Input,
+    OnInit,
+    DoCheck
 } from '@angular/core';
 
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
@@ -14,115 +14,115 @@ import { SystemOfUnits } from './system-of-units.service';
 
 
 const CONTROL_VALUE_ACCESSOR = {
-  name: 'ngQuantityValueAccessor',
-  provide: NG_VALUE_ACCESSOR,
-  useExisting: forwardRef(() => QuantityDirective),
-  multi: true
+    name: 'ngQuantityValueAccessor',
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: forwardRef(() => QuantityDirective),
+    multi: true
 };
 
 @Directive({
-  selector: '[ngQuantity]',
-  providers: [
-      CONTROL_VALUE_ACCESSOR
-  ]
+    selector: '[ngQuantity]',
+    providers: [
+        CONTROL_VALUE_ACCESSOR
+    ]
 })
 export class QuantityDirective implements ControlValueAccessor, OnInit, DoCheck {
 
-  @Input()
-  formControlName: string;
+    @Input()
+    formControlName: string;
 
-  @Input('ngQuantity')
-  quantityAttr: string | Quantity;
+    @Input('ngQuantity')
+    quantityAttr: string | Quantity;
 
-  quantity: Quantity;
+    quantity: Quantity;
 
-  private inputElement: HTMLInputElement;
-  private onTouch: Function;
-  private onModelChange: Function;
-  private currentUnit: string;
-  private currentModelValue;
+    private inputElement: HTMLInputElement;
+    private onTouch: Function;
+    private onModelChange: Function;
+    private currentUnit: string;
+    private currentModelValue;
 
-  constructor(private elementRef: ElementRef, private service: SystemOfUnits) {
+    constructor(private elementRef: ElementRef, private service: SystemOfUnits) {
 
-  }
+    }
 
-  ngOnInit(): void {
-      this.inputElement = this.getInputElement();
-      // this.quantity = typeof this.quantityAttr === 'string' ? 
-      //     this.service.getQuantity(this.quantityAttr) : this.quantityAttr;
+    ngOnInit(): void {
+        this.inputElement = this.getInputElement();
+        // this.quantity = typeof this.quantityAttr === 'string' ? 
+        //     this.service.getQuantity(this.quantityAttr) : this.quantityAttr;
 
-      this.quantity = this.quantityAttr as Quantity;
-      if (!this.quantity) {
-          throw new Error('Invalid quantity.');
-      }
-  }
+        this.quantity = this.quantityAttr as Quantity;
+    }
 
-  ngDoCheck(): void {
-      let newUnit = this.quantity.unit.symbol;
-      if (newUnit !== this.currentUnit) {
-          this.updateView(this.currentModelValue);
-          this.currentUnit = newUnit;
-      }
-  }
+    ngDoCheck(): void {
+        this.quantity = this.quantityAttr as Quantity;
+        let newUnit = this.quantity ? this.quantity.unit.symbol : '';
+        if (newUnit !== this.currentUnit) {
+            this.updateView(this.currentModelValue);
+            this.currentUnit = newUnit;
+        }
+    }
 
-  registerOnTouched(fn) {
-      this.onTouch = fn;
-  }
+    registerOnTouched(fn) {
+        this.onTouch = fn;
+    }
 
-  registerOnChange(fn) {
-      this.onModelChange = fn;
-  }
+    registerOnChange(fn) {
+        this.onModelChange = fn;
+    }
 
+    // Parser: View to Model
+    @HostListener('input', ['$event'])
+    onControlInput($event: KeyboardEvent) {
 
-  // Parser: View to Model
-  @HostListener('input', ['$event'])
-  onControlInput($event: KeyboardEvent) {
+        const rawValue: any = this.inputElement.value;
+        const modelValue = this.quantity ? this.quantity.toBase(rawValue) : rawValue;
+        this.currentModelValue = modelValue;
 
-      const rawValue: any = this.inputElement.value;
-      const modelValue = this.quantity.toBase(rawValue);
-      this.currentModelValue = modelValue;
+        if (this.onTouch) {
+            this.onTouch();
+        }
 
-      if (this.onTouch) {
-          this.onTouch();
-      }
-
-      if (this.onModelChange) {
-          this.onModelChange(modelValue);
-      }
-
-  }
+        if (this.onModelChange) {
+            this.onModelChange(modelValue);
+        }
+    }
 
 
-  // Formatter: Model to View
-  writeValue(rawValue: any): void {
-      this.currentModelValue = rawValue;
-      this.updateView(rawValue);
-  }
+    // Formatter: Model to View
+    writeValue(rawValue: any): void {
+        this.currentModelValue = rawValue;
+        this.updateView(rawValue);
+    }
 
-  private updateView(modelValue: any) {
-      let converted = this.quantity.fromBase(modelValue);
-      if (converted !== null && converted !== undefined) {
-          this.inputElement.value = this.quantity.print(converted, false);
-      }
-      else {
-          this.inputElement.value = null;
-      }
-  }
+    private updateView(modelValue: any) {
+        if (!this.quantity) {
+            this.inputElement.value = modelValue;
+            return;
+        }
+        let converted = this.quantity.fromBase(modelValue);
+        if (converted !== null && converted !== undefined) {
+            this.inputElement.value = this.quantity.print(converted, false);
+        }
+        else {
+            this.inputElement.value = null;
+        }
+    }
 
-  private getInputElement(): HTMLInputElement {
-      let input: HTMLInputElement;
-      let element = this.elementRef.nativeElement;
-      if (element.tagName === 'INPUT') {
-          input = element;
-      } 
-      else {
-          input = element.querySelector('input');
-      }
+    private getInputElement(): HTMLInputElement {
+        let input: HTMLInputElement;
+        let element = this.elementRef.nativeElement;
+        if (element.tagName === 'INPUT') {
+            input = element;
+        }
+        else {
+            input = element.querySelector('input');
+        }
 
-      if (!input) {
-          throw new Error('ngQuantity only allowed on inputs or elements containing inputs.');
-      }
-      return input;
-  }
+        if (!input) {
+            throw new Error('ngQuantity only allowed on inputs or elements containing inputs.');
+        }
+        return input;
+    }
 
 }
