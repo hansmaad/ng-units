5948ec4ac23de053153d84fa0b0ff76cd94f6a06
Kai Nadler <kai.nadler@oerlikon.com>
2018-01-26 13:13:14 +0100

    feat: Add volume quantity.

--- src/app/modules/ng-units/quantities/index.ts
+++ src/app/modules/ng-units/quantities/index.ts
@@ -1,2 +1,3 @@
 export * from './length';
 export * from './area';
+export * from './volume';
---
+++ src/app/modules/ng-units/quantities/volume.ts
@@ -0,0 +1,8 @@
+export var volume = {
+    name: 'Volume',
+    units: {
+        'm³' : [1],
+        'cm³': [1000000],
+        'mm³': [1000000000]
+    }
+};
