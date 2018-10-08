339d0b72c0ce9cef341f466279e4656876ed49c6
hansmaad <hansmaad@gmail.com>
2018-10-08 20:22:05 +0200

    Export pressure quantity

--- src/app/modules/ng-units/quantities/index.ts
+++ src/app/modules/ng-units/quantities/index.ts
@@ -2,6 +2,7 @@ export * from './area';
 export * from './frequency';
 export * from './length';
 export * from './mass';
+export * from './pressure';
 export * from './temperature';
 export * from './time';
 export * from './volume';
--- src/app/system-of-units-initializer.ts
+++ src/app/system-of-units-initializer.ts
@@ -1,10 +1,10 @@
 import { 
     SystemOfUnits, Quantity, SystemOfUnitsConfig, 
-    length, area, temperature, time } from "../../public_api";
+    length, area, temperature, time, pressure } from "../../public_api";
 
 
 export function systemOfUnitsInitializer(): SystemOfUnitsConfig {
     return {
-        quantities: [area, length, time, temperature]
+        quantities: [area, length, time, temperature, pressure]
     }
 }
